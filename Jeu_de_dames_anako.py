9e5b650967e096e6c871111e7cfe1aa498ec273f
akoan60 <anaelakue60@gmail.com>
2019-10-29 15:22:36 +0100

    All cell filed with black pawn

--- Jeu_de_dames_anako.py
+++ Jeu_de_dames_anako.py
@@ -9,12 +9,13 @@ from tkinter import *
 from tkinter import messagebox
 
 class Checkboard():
-    def __init__(self):
-        self.CELL_SIZE = 60
-        self.NUMBER_CELL = 10
+    CELL_SIZE = 60
+    NUMBER_CELL = 10
+
+    def __init__(self, canvas):
+        self.canvas = canvas
 
     def drawgrid(self, canvas):
-        color = 'navajowhite'
         for x in range(self.NUMBER_CELL):
             if x % 2 == 0:
                 color_change = 0
@@ -28,12 +29,15 @@ class Checkboard():
                 canvas.create_rectangle(x * self.CELL_SIZE, y * self.CELL_SIZE,
                                         x * self.CELL_SIZE + self.CELL_SIZE,
                                         y * self.CELL_SIZE + self.CELL_SIZE, fill=color)
+        self.placePawn()
 
     def clic(self, event):
         pass
 
     def placePawn(self):
-        pass
+        for x in range(Checkboard.NUMBER_CELL):
+            for y in range(Checkboard.NUMBER_CELL):
+                pawn = Pawn(self.canvas, x, y, "black")
 
     def clear(self, canvas):
         # Nettoyage du canevas
@@ -43,6 +47,7 @@ class Checkboard():
             canvas.delete("all")
             d.drawgrid(can1)
 
+
 class Pawn():
     def __init__(self, canvas, x, y, color):
         self.canvas = canvas
@@ -50,17 +55,26 @@ class Pawn():
         self.y = y
         self.color = color
         self.size = 7
-        self.pawn = self.canvas.create_oval()
+        self.coin = self.canvas.create_oval(self.x * Checkboard.CELL_SIZE, self.y * Checkboard.CELL_SIZE,
+                                            self.x * Checkboard.CELL_SIZE + Checkboard.CELL_SIZE,
+                                            self.y * Checkboard.CELL_SIZE + Checkboard.CELL_SIZE,
+                                            fill = self.color)
 
-class Player():
-    def __init__(self):
+    def become_queen(self):
         pass
 
+
+class Player():
+    def __init__(self, couleur):
+        self.couleur = couleur
+
+
 def leave():
-    answer =  messagebox.askyesno('Close the game',
-                                    'You are about to close the game, are you sure ?')
+    answer = messagebox.askyesno('Close the game',
+                                 'You are about to close the game, are you sure ?')
     if answer:
         window1.quit()
+
 
 # Création du widget principal
 window1 = Tk()
@@ -71,7 +85,7 @@ can1 = Canvas(window1, bg='dark grey', height=600, width=600)
 can1.pack(side=LEFT)
 
 # Creation du damier
-d = Checkboard()
+d = Checkboard(can1)
 d.drawgrid(can1)
 
 # Création des boutons de controle
